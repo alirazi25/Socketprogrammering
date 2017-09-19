e5000ffc5c6cddb1c73bdeb6ade0208bbad8d515
alirazi25 <alirazi2512@gmail.com>
2017-09-19 18:41:25 +0200

    GUI fxml og gui klasse - mangler stage start

---
+++ src/GUIclient.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+
+
+
+    public class GUIclient extends Application {
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+
+
+        }
+
+
+
